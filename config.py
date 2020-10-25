e65177466c3c49afaa27cf2ce993616ea02b8ff7
Alexandr Lenetc <43437466+ale-alexandro@users.noreply.github.com>
2020-10-25 17:18:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ def generator(length):
 
 class Config(object):
     SECRET_KEY = generator(32)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
