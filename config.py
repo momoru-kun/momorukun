9563a9840d1bc3b09998380bec93f4ad88fa6fe8
Alexandr Lenetc <43437466+ale-alexandro@users.noreply.github.com>
2020-10-25 17:12:02 +0300

    Update config.py
    On Heroku we need to log to stdout

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     SECRET_KEY = generator(32)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
