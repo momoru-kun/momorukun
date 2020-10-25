06705661ffacd6fdf63ee511e88643686561e7f5
Alexandr Lenetc <43437466+ale-alexandro@users.noreply.github.com>
2020-10-25 17:17:16 +0300

    Added stdout logging

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
 
+import logging
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+
 app = Flask(__name__)
 app.config.from_object(Config)
+app.logger.addHandler(stream_handler)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 socketio = SocketIO(app, async_mode=None)
